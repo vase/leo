4f4819f159c0e0f134495fc299eefd8493be870b
Asyrique Thevendran <asyrique@gmail.com>
2017-01-13 14:10:43 +0800

    add more prod

---
+++ dist/js/snippet.js
@@ -0,0 +1,3 @@
+!function(e,t,n){var o=(e.Leo||{},t.createElement("script"));o.type="text/javascript",
+o.async=!0,o.src="leo.js",o.onload=function(){e.Leo.init(),n()};var i=t.getElementsByTagName("script")[0];
+i.parentNode.insertBefore(o,i)}(window,document,function(){Leo.logEvent("visit")});
